b15e72f05cc4fd2f110947d2cf7b832afd2a0179
Daniyal Zafar <daniyalzafarm@gmail.com>
2025-11-16 01:35:36 +0500

    feat(ui-web): updated input styles

--- packages/ui-web/src/components/input.tsx
+++ packages/ui-web/src/components/input.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Input as HeroInput, type InputProps } from '@heroui/react';
-import { cn } from '../utils/cn';
+import React from 'react'
+import { Input as HeroInput, type InputProps } from '@heroui/react'
+import { cn } from '../utils/cn'
 
 export interface CustomInputProps extends Omit<InputProps, 'classNames'> {
-  classNames?: InputProps['classNames'];
+  classNames?: InputProps['classNames']
 }
 
-export const Input: React.FC<CustomInputProps> = ({
-  classNames: customClassNames,
-  ...props
-}) => {
+export const Input: React.FC<CustomInputProps> = ({ classNames: customClassNames, ...props }) => {
   const mergedClassNames = {
     ...customClassNames,
     ...{
       inputWrapper: cn(
-        'rounded-md bg-white p-4 py-5.5',
-        'border border-gray-300 dark:border-gray-600',
-        'focus-within:border-gray-400',
-        'focus-within:bg-gray-50',
-        'data-[hover=true]:bg-gray-100',
-        customClassNames?.inputWrapper,
+        'rounded-lg bg-white',
+        'border border-gray-200',
+        'hover:border-gray-300',
+        'focus-within:border-primary',
+        'focus-within:!bg-white',
+        'dark:border-gray-600',
+        customClassNames?.inputWrapper
       ),
-      input: cn('text-md', customClassNames?.input),
+      input: cn(customClassNames?.input),
     },
-  };
+  }
 
-  return <HeroInput {...props} classNames={mergedClassNames} />;
-};
+  return <HeroInput {...{ size: 'lg', ...props }} classNames={mergedClassNames} />
+}
